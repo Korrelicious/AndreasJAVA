0458705c3cab8fe5a19df8b51674e008dbc30545
NadiiaOn <nadiia.onopriienko@gmail.com>
2025-11-18 13:45:20 +0100

    add points and List <Question>

--- src/Quiz.java
+++ src/Quiz.java
@@ -1,6 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Quiz {
+    int points;
+
+    List<Question> questions = new ArrayList<Question>();
+
     public static void main(String[] args) {
 
     }
