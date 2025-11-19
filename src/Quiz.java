c5b40e9dc2d8aa198658350f7066ae0375a2bfe8
admirfmt <admir.fmt@gmail.com>
2025-11-19 13:37:34 +0100

    quiz logic - for loop

--- src/Quiz.java
+++ src/Quiz.java
@@ -1,19 +1,24 @@
 import java.util.ArrayList;
 import java.util.List;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Quiz {
 
     public static void main(String[] args) {
 
         int points = 0;
 
-        List<Question> questions = new ArrayList<Question>();
+        List<Question> questions = new ArrayList<>();
 
-        questions.add(new Question("What is the capital of Sweden?\n", "1.Malmo;\n 2.Stockholm;\n 3.Karlskrona;\n 4.Karlshamn; \n", "2.Stockholm\n"));
-        questions.add(new Question("What is the capital of United Kingdom?\n", "1.London;\n 2.Liverpool;\n 3.Cardiff;\n 4.Bradford;\n", "1.London\n"));
-        questions.add(new Question("What is the capital of Ukraine?\n", "1.Lviv;\n 2.Odesa;\n 3.Kyiv;\n 4.Kharkiv\n", "3.Kyiv\n"));
-        System.out.println(questions);
+        questions.add(new Question("What is the capital of Sweden?\n", "1.Malmo;\n2.Stockholm;\n3.Karlskrona;\n4.Karlshamn;\n", "Stockholm"));
+        questions.add(new Question("What is the capital of United Kingdom?\n", "1.London;\n2.Liverpool;\n3.Cardiff;\n4.Bradford;\n", "London"));
+        questions.add(new Question("What is the capital of Ukraine?\n", "1.Lviv;\n2.Odesa;\n3.Kyiv;\n4.Kharkiv;\n", "Kyiv"));
+
+        for (Question q : questions) {
+            System.out.println(q.getQuestion());
+            System.out.println(q.getAnswer());
+            System.out.println("Enter your answer: ");
+
+        }
+        System.out.println("Quiz finished. Your score: " + points);
     }
 }
