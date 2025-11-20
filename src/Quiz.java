7f5d6057d194c98b28a0267e4e63991fcf3357cb
Magda-blip <82.magdalena@gmail.com>
2025-11-20 17:36:20 +0100

    Merge branch 'main' of https://github.com/NadiiaOn/Group2-Java-

--- src/Quiz.java
+++ src/Quiz.java
@@ -1,9 +1,11 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Quiz {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
         int points = 0;
 
@@ -13,11 +15,15 @@ public class Quiz {
         questions.add(new Question("What is the capital of United Kingdom?\n", "1.London;\n2.Liverpool;\n3.Cardiff;\n4.Bradford;\n", "London"));
         questions.add(new Question("What is the capital of Ukraine?\n", "1.Lviv;\n2.Odesa;\n3.Kyiv;\n4.Kharkiv;\n", "Kyiv"));
 
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Welcome " + name);
+
         for (Question q : questions) {
             System.out.println(q.getQuestion());
             System.out.println(q.getAnswer());
             System.out.println("Enter your answer: ");
-
+            String answer = scanner.nextLine();
             String answerInput = scanner.nextLine();
             if (answerInput.equalsIgnoreCase(q.getCorrectAnswer())) {
                 System.out.println("Correct!");
@@ -32,4 +38,6 @@ public class Quiz {
 
         System.out.println("Quiz finished. Your score: " + points);
     }
+
+
 }
